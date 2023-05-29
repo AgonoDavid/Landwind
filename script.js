e7b6ea35482e3ce16c27ca86ac5699e4ead439b7
AgonoDavid <davidagono239@gmail.com>
2023-05-29 17:26:26 +0100

    new

--- script.js
+++ script.js
@@ -6,7 +6,18 @@ const mobile_view = document.querySelector(".nav-middle");
 
 const navbar = document.querySelector(".nav-bar");
 
+const up_arrow = document.querySelector(".faq-arrow");
+
+const faq_text = document.querySelector(".faq-test");
+
+const closehamburger = document.querySelector(".closehamburger");
+
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("active");
   mobile_view.classList.toggle("active");
+  // closehamburger.classList.remove("hidden");
+});
+
+up_arrow.addEventListener("click", function () {
+  faq_text.classList.toggle("hidden");
 });
