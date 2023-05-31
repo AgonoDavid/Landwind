fd35a62a149caff12ccd727a1a85651ccd82c927
AgonoDavid <davidagono239@gmail.com>
2023-05-31 16:06:47 +0100

    new

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ const mobile_view = document.querySelector(".nav-middle");
 
 const navbar = document.querySelector(".nav-bar");
 
-const up_arrow = document.querySelector(".faq-arrow");
-
 const faq_text = document.querySelector(".faq-test");
-
+const up_arrow = document.querySelector(".faq-arrow");
 const closehamburger = document.querySelector(".closehamburger");
+
+const arrow = document.getElementById("arrow-up");
+
+let toggle = true;
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("active");
@@ -20,4 +22,9 @@ hamburger.addEventListener("click", function () {
 
 up_arrow.addEventListener("click", function () {
   faq_text.classList.toggle("hidden");
+  toggle = !toggle;
+
+  toggle
+    ? (up_arrow.src = "images\\arrow-up.png")
+    : (up_arrow.src = "images\\arrow-down.png");
 });
